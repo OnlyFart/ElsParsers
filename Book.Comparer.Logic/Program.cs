a58c5ba516921bf1ae47dbc2d5ecbaed4f58a211
oleg.koloskov <oleg.koloskov@ingate.ru>
2021-01-25 13:17:42 +0300

    почистил

--- Book.Comparer.Logic/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Book.Comparer.Logic {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
