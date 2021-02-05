3ee26c1fe7d5f876cedbf5da853c7f2634a7acea
dmitriy.torgov <dmitriy.torgov@ingate.ru>
2021-02-05 16:16:42 +0300

    закинул оставшиеся

--- Parsers/AcademiaMoscow.Parser/Configs/IParserConfig.cs
+++ Parsers/AcademiaMoscow.Parser/Configs/IParserConfig.cs
@@ -2,8 +2,5 @@ using Parser.Core.Configs;
 
 namespace AcademiaMoscow.Parser.Configs {
     public interface IParserConfig : IParserConfigBase {
-        int StartIndex { get; set; }
-        
-        int EndIndex { get; set; }
     }
 }
--- Parsers/AcademiaMoscow.Parser/Configs/Options.cs
+++ Parsers/AcademiaMoscow.Parser/Configs/Options.cs
@@ -4,10 +4,5 @@ using Parser.Core.Configs;
 
 namespace AcademiaMoscow.Parser.Configs {
     public class Options : OptionsBase, IParserConfig {
-        [Option("si", Required = false, HelpText = "Начальный индекс для обхода", Default = 1)]
-        public int StartIndex { get; set; }
-        
-        [Option("ei", Required = false, HelpText = "Конечный индекс для обхода", Default = 1000000)]
-        public int EndIndex { get; set; }
     }
 }
