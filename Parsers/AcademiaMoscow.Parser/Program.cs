d7302f92e6a9e8398718a675c2e9a35da51f1e8f
dmitriy.torgov <dmitriy.torgov@ingate.ru>
2021-02-05 14:51:02 +0300

    лишнее убрал

--- Parsers/AcademiaMoscow.Parser/Program.cs
+++ Parsers/AcademiaMoscow.Parser/Program.cs
@@ -9,13 +9,6 @@ using AcademiaMoscow.Parser.Configs;
 namespace AcademiaMoscow.Parser {
     class Program {
         private static async Task Main(string[] args) {
-            args = new string [] { "--cs",  "mongodb+srv://library123:library123@cluster0.sng0y.mongodb.net/library?retryWrites=true&w=majority",  "--th", "10"};
-
-            // var options = CommandLine.Parser.Default.ParseArguments<Options>(args);
-            // var kernel = new StandardKernel(new CoreNinjectModule(options));
-            // var parser = new Logic.Parser(new Options(), null);
-            
-            //--cs "mongodb+srv://library123:library123@Test?retryWrites=true&w=majority" --th 10
             await CommandLine.Parser.Default.ParseArguments<Options>(args)
                 .WithParsedAsync(async options => {
                     var kernel = new StandardKernel(new CoreNinjectModule(options));
