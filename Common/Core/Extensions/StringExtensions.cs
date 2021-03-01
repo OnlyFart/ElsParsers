cd574d96568d388ef48c0c30e2d41ffaec0e6475
oleg.koloskov <oleg.koloskov@ingate.ru>
2021-03-01 14:01:38 +0300

    Обработка nullref

--- Common/Core/Extensions/StringExtensions.cs
+++ Common/Core/Extensions/StringExtensions.cs
@@ -5,7 +5,8 @@ namespace Core.Extensions {
     public static class StringExtensions {
         public static string Clean(this string str) {
             var sb = new StringBuilder();
-            foreach (var c in str) {
+
+            foreach (var c in str ?? string.Empty) {
                 if (char.IsLetter(c) || char.IsWhiteSpace(c) || c == '.') {
                     sb.Append(c);
                 } else {
