780fa39061859c59c3ec2ae60385467217e01d07
oleg.koloskov <oleg.koloskov@ingate.ru>
2021-03-01 17:41:49 +0300

    Логи

--- Comparer/Book.Comparer/Logic/Comparer.cs
+++ Comparer/Book.Comparer/Logic/Comparer.cs
@@ -113,7 +113,7 @@ namespace Book.Comparer.Logic {
             var i = books.Count(b => b.BookInfo.Compared);
             var updateBooks = new ActionBlock<SaveResult>(async t => {
                 await _similarSaver.Save(t);
-                _logger.Info($"Обработано {++i}/{books.Count}");
+                _logger.Info($"Обработано {++i}/{books.Count}. ID = {t.Book.Id} Name = {t.Book.Name}");
             });
             updateBooks.CompleteMessage(_logger, "Сохранение завершено.");
 
